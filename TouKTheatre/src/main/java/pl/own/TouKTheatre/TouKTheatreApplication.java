81c87171af5f9abe0ff25539bdc178f6f110be86
MrCosik <56444584+MrCosik@users.noreply.github.com>
2021-07-07 19:10:46 +0200

    First commit

---
+++ TouKTheatre/src/main/java/pl/own/TouKTheatre/TouKTheatreApplication.java
@@ -0,0 +1,13 @@
+package pl.own.TouKTheatre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TouKTheatreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TouKTheatreApplication.class, args);
+	}
+
+}
---
+++ TouKTheatre/src/test/java/pl/own/TouKTheatre/TouKTheatreApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.own.TouKTheatre;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TouKTheatreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
