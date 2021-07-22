b68c19ee48186939b8910dd90b05e8c67a65e55d
MrCosik <56444584+MrCosik@users.noreply.github.com>
2021-07-22 20:17:30 +0200

    Added spring data rest and annotations in repositories to RepositoryRestResource

--- src/main/java/pl/own/TouKTheatre/repository/MovieRepository.java
+++ src/main/java/pl/own/TouKTheatre/repository/MovieRepository.java
@@ -3,9 +3,10 @@ package pl.own.TouKTheatre.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.CrudRepositoryExtensionsKt;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
 import pl.own.TouKTheatre.entity.Movie;
 
-@Repository
+@RepositoryRestResource
 public interface MovieRepository extends JpaRepository<Movie, Long> {
 }
--- src/main/java/pl/own/TouKTheatre/repository/RoomRepository.java
+++ src/main/java/pl/own/TouKTheatre/repository/RoomRepository.java
@@ -1,11 +1,10 @@
 package pl.own.TouKTheatre.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-import org.springframework.stereotype.Repository;
 import pl.own.TouKTheatre.entity.Movie;
+import pl.own.TouKTheatre.entity.Room;
 
 @RepositoryRestResource
-public interface UserRepository extends JpaRepository<Movie, Long> {
+public interface RoomRepository extends JpaRepository<Room, Long> {
 }
